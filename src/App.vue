d3dabe212675ab3934304083bc805d6eadfa1c44
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2026-02-11 20:41:34 +0200

    Implement product fetching and add Product interface

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Product } from './Product'
+import { ref } from 'vue'
+
+const products = ref<Product[]>([]);
+const isLoading = ref(true);
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    products.value = await response.json();
+    console.log('Fetched products:', products.value);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+fetchProducts();
+
+</script>
 
 <template>
   <h1>Products</h1>
+  <div v-if="isLoading">Loading products... </div>
+  <ul>
+    <li v-for="product in products" :key="product.id">
+      {{ product.title }} - ${{ product.price }}
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
